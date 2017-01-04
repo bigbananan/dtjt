5ee00bd38a53054afeccac5c93a9058608429d1a
jeonlee <elvanjeonlee89@gmail.com>
2017-01-04 10:10:19 +0700

    Update userlist.blade.php

--- resources/views/index/userlist.blade.php
+++ resources/views/index/userlist.blade.php
@@ -1,40 +1,62 @@
-@extends('layouts.layout')
+@extends('layouts.index')
 
 @section('content')
 
-<div class="panel panel-default">
-        <div class="panel-heading">
-          <div class="panel-title">
-            User Lists
-          </div>
-        </div>
+<div class="panel panel-primary">
+                <div class="panel-heading p15 black font">
+                  <div class="panel-title">
+                    <h3>LIST USER</h3>
+                  </div>
+                </div>
 
-        <div class="panel-body">
-            <table class="table table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th>User</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
+                <div class="panel-body">
+                  <table class="middle">
+                    <thead>
+                      <tr class="kolom red">
+                                                           
+                        <th><div class="th_jam">DATE</div></th>
+                        <th><div class="th_jam">PERIODE</div></th>
+                        <th><div class="th_jam">RESULT</div></th>
+                      </tr>
+                    </thead>
+                    <tbody>
                     @foreach ($users as $user)
-                        <tr>
-                            
-                            <td>{{ $user->name }}</td>
-                            
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="text-center">
+                      <tr>
+                        <td><div class="t_no f20">{{ $user->name }}</div></td>          
+                        
+                      </tr>   
+                      
+                      @endforeach
+                                                                                                                                                                                                                                                   
+                    </tbody>
+                
+                
+                </table>
+                <div class="text-center">
                 {!! $users->render(); !!}
             </div>
-        </div>
-    </div>
+                </div>
+              </div>
 
 @endsection
 
-@section('footer')
-    Footer result
+@section('numbergenerator')
+
+
+              <div class="panel panel-default" style="margin-top:15px;">
+                <div class="panel-heading pink">
+                  <h3 class="panel-title white">NUMBER GENERATOR</h3>
+                </div>
+                <div class="panel-body p15">
+                  <p class="helvetica" style="text-align:center;">Generate your numbers</p>
+                  <br>
+                  <a title="Lottery Results - Lotto" class="button-blue"><button style="border:none; background:transparent;">Generate Numbers</button></a><br><br>
+                  <div class="group">
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                  </div>
+                </div>
+              </div>
 @endsection
